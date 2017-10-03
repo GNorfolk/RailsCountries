5ffd16f3c1f0fb3db2a43fe5ac245d430484c9a2
GNorfolk <georgenorfolk@hotmail.co.uk>
2017-10-03 11:14:17 +0100

    Final commit of homework with everything working.

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -8,18 +8,38 @@ class CountriesController < ApplicationController
   end
 
   def create
+    new_country = Country.new
+    new_country.params[:name]
+    new_country.params[:population]
+    new_country.params[:flag_url]
+    new_country.params[:language]
+    new_country.params[:president]
+    new_country.save
+    redirect_to "/countries"
   end
 
   def show
+    @country = Country.find(params[:id])
   end
 
   def edit
+    @country = Country.find(params[:id])
   end
 
   def update
+    up_country = Country.find(params[:id])
+    up_country.params[:name]
+    up_country.params[:population]
+    up_country.params[:flag_url]
+    up_country.params[:language]
+    up_country.params[:president]
+    up_country.save
+    redirect_to "/countries"
   end
 
   def destroy
+    Country.delete(params[:id])
+    redirect_to "/countries"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,10 @@
 
 # name:text population:integer flag_url:text language:text president:text
 
-Country.create!(name: "Javatopia", population: 3249238, flag_url: "URL", language: "JavaScript", president: "Niall")
-Country.create!(name: "Railicia", population: 932132, flag_url: "URL", language: "Ruby", president: "Jenny")
-Country.create!(name: "Rubionia", population: 1405216, flag_url: "URL", language: "Ruby", president: "Gemima")
-Country.create!(name: "HTMLand", population: 8129357, flag_url: "URL", language: "HTML", president: "BootstrapBrian")
-Country.create!(name: "CSScotia", population: 7749634, flag_url: "URL", language: "CSS", president: "JackJack")
+Country.create!(name: "Javatopia", population: 3249238, flag_url: "http://flag-designer.appspot.com/gwtflags/SvgFileService?d=0&c1=2&c2=2&c3=2&o=4&c4=1&s=5&c5=1", language: "JavaScript", president: "Niall")
+Country.create!(name: "Railicia", population: 932132, flag_url: "http://flag-designer.appspot.com/gwtflags/SvgFileService?d=5&c1=3&c2=5&c3=2&o=0&c4=1&s=10&c5=6", language: "Ruby", president: "Jenny")
+Country.create!(name: "Rubionia", population: 1405216, flag_url: "http://flag-designer.appspot.com/gwtflags/SvgFileService?d=5&c1=5&c2=5&c3=2&o=11&c4=3&s=10&c5=6", language: "Ruby", president: "Gemima")
+Country.create!(name: "HTMLand", population: 8129357, flag_url: "http://flag-designer.appspot.com/gwtflags/SvgFileService?d=3&c1=1&c2=7&c3=1&o=6&c4=3&s=15&c5=1", language: "HTML", president: "BootstrapBrian")
+Country.create!(name: "CSScotia", population: 7749634, flag_url: "http://flag-designer.appspot.com/gwtflags/SvgFileService?d=3&c1=2&c2=4&c3=3&o=5&c4=1&s=0&c5=1", language: "CSS", president: "JackAttack")
 
 puts "Finished Seed ....."
